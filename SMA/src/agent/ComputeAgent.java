55682b83c5a7d05b31dde5d45af59f5a493bed98
EnzoKalinowski <enzo.kalinowski@gmail.com>
2022-12-11 19:08:40 +0100

    ComputeAgent finalize

--- SMA/src/agent/ComputeAgent.java
+++ SMA/src/agent/ComputeAgent.java
@@ -6,6 +6,7 @@ import jade.core.behaviours.*;
 import jade.core.Agent;
 import jade.lang.acl.*;
 
+
 public class ComputeAgent extends Agent{
 	protected Function f;
 	
@@ -16,10 +17,24 @@ public class ComputeAgent extends Agent{
 				ACLMessage msg =receive();
 				if(msg!=null) {
 					System.out.println(" - "+ myAgent.getLocalName()+" <- "+ msg.getContent());
+					
 					String[] splitMsg= msg.getContent().split(",");
 					
-					f=new MyFunction(Double.parseDouble(splitMsg[0]), Double.parseDouble(splitMsg[1]), Double.parseDouble(splitMsg[2]));
-				
+					double min = Double.parseDouble(splitMsg[1]);
+					double max = Double.parseDouble(splitMsg[2]);
+					double delta = Double.parseDouble(splitMsg[3]);
+					
+					switch (splitMsg[0]) {
+					
+						case "MyFunction": {
+							f=new MyFunction(Double.parseDouble(splitMsg[0]), Double.parseDouble(splitMsg[1]), Double.parseDouble(splitMsg[2]));
+	
+							break;
+						}
+						default:
+							throw new IllegalArgumentException("Unexpected value: " + splitMsg[0]);
+					}
+					
 					double computeResult=f.eval();
 					
 					ACLMessage reply = msg.createReply();
