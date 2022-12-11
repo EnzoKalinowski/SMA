297fd80393bf8914a2dd06702555cc8c0c3b80c5
ewenn-s <60225310+ewenn-s@users.noreply.github.com>
2022-12-11 20:17:27 +0100

    ajout pas du tout complet

--- SMA/src/agent/TestParallelAgent.java
+++ SMA/src/agent/TestParallelAgent.java
@@ -5,6 +5,7 @@ import jade.core.behaviours.*;
 import jade.domain.FIPAAgentManagement.*;
 import jade.lang.acl.*;
 import jade.domain.*;
+import functions.Function;
 
 public class TestParallelAgent extends Agent{
 	
@@ -25,22 +26,23 @@ public class TestParallelAgent extends Agent{
 		      DFAgentDescription dfd = new DFAgentDescription();
 		      DFAgentDescription[] result = DFService.search(this, dfd);
 
-		      System.out.println("Generic search returns: " + result.length 
+		      /*System.out.println("Generic search returns: " + result.length 
 		                         + " elements" );
 		      if (result.length>0)
 		        System.out.println(" " + result[0].getName() );
-
+		      */
 		      // looking for calculateur service : return current agent
 		      sd  = new ServiceDescription();
 		      sd.setType( "calculateur" );
 		      dfd.addServices(sd);
 		      result = DFService.search(this, dfd);
 		      System.out.println("Search for calculateur: " + result.length + " elements" );
+		      int nb_agent_trouve = result.length;
 		      if (result.length>0)
 		        System.out.println(" " + result[0].getName() );
-
+		      
 		      // finally, looking for (non-existent) SELLER agent
-		      sd.setType( "seller" );
+		      /*sd.setType( "seller" );
 		      result = DFService.search(this, dfd);
 		      if (result==null) System.out.println("Search1 returns null");
 		      else {
@@ -48,9 +50,24 @@ public class TestParallelAgent extends Agent{
 		                           + " elements" );
 		        if (result.length>0)
 		          System.out.println(" " + result[0].getName() );
-		      }
+		      }*/
+			    int min = 0;
+			    int max = 10;
+			    double delta = 0.1;
+			    float pas = max-min;
+			    if (nb_agent_trouve != 0) {
+			    	pas = (max-min)/nb_agent_trouve;
+			    }
+			    float minLoc = 0;
+			    float maxloc = 0;
+			    for(int i = 0; i < result.length; i++){
+			    	maxloc = minLoc + pas;
+			    }
+			    
 		    }
 		    catch (FIPAException fe) { fe.printStackTrace(); }
+		    
+		    
 
 		    System.exit(0);
 		  }
@@ -69,5 +86,39 @@ public class TestParallelAgent extends Agent{
 		    }
 		    catch (FIPAException fe) { fe.printStackTrace(); }
 	  }
+		  
+}
 
+class SendAndRecieve extends SimpleBehaviour{
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
+
+class Calculate extends SimpleBehaviour{
+	protected Function f;
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		double computeResult=f.eval();
+		
+	}
+
+	@Override
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }
