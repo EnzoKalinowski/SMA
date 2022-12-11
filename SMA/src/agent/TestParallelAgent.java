c8f13807b3b10b72d6493135a144614d83a81ca1
ewenn-s <60225310+ewenn-s@users.noreply.github.com>
2022-12-11 18:02:48 +0100

    modif de la jar

--- SMA/src/agent/TestParallelAgent.java
+++ SMA/src/agent/TestParallelAgent.java
@@ -1,20 +1,73 @@
 package agent;
 
-import jade.core.Agent;
+import jade.core.*;
 import jade.core.behaviours.*;
+import jade.domain.FIPAAgentManagement.*;
 import jade.lang.acl.*;
+import jade.domain.*;
 
 public class TestParallelAgent extends Agent{
-	protected void setup() {
-		addBehaviour(new CyclicBehaviour() {
-			
-			@Override
-			public void action() {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-	}
 	
+	  protected void setup() {
+		    // registering calculateur service for current agent
+		    ServiceDescription sd  = new ServiceDescription();
+		    try {
+		        sd.setType( getArguments()[0].toString() );
+		    }
+		    catch(Exception e){
+		        sd.setType( "calculateur" );
+		    }
+		    sd.setName( getLocalName() );
+		    register( sd );
+
+		    try {
+		      // looking for any service : return current agent
+		      DFAgentDescription dfd = new DFAgentDescription();
+		      DFAgentDescription[] result = DFService.search(this, dfd);
+
+		      System.out.println("Generic search returns: " + result.length 
+		                         + " elements" );
+		      if (result.length>0)
+		        System.out.println(" " + result[0].getName() );
+
+		      // looking for calculateur service : return current agent
+		      sd  = new ServiceDescription();
+		      sd.setType( "calculateur" );
+		      dfd.addServices(sd);
+		      result = DFService.search(this, dfd);
+		      System.out.println("Search for calculateur: " + result.length + " elements" );
+		      if (result.length>0)
+		        System.out.println(" " + result[0].getName() );
+
+		      // finally, looking for (non-existent) SELLER agent
+		      sd.setType( "seller" );
+		      result = DFService.search(this, dfd);
+		      if (result==null) System.out.println("Search1 returns null");
+		      else {
+		        System.out.println("Search for SELLER: " + result.length 
+		                           + " elements" );
+		        if (result.length>0)
+		          System.out.println(" " + result[0].getName() );
+		      }
+		    }
+		    catch (FIPAException fe) { fe.printStackTrace(); }
+
+		    System.exit(0);
+		  }
+
+		  /**
+		   * Useful wrapper for registering services : hides the usage of the
+		   * DFD
+		   */
+		  void register( ServiceDescription sd) {
+		    DFAgentDescription dfd = new DFAgentDescription();
+		    dfd.setName(getAID());
+		    dfd.addServices(sd);
+
+		    try {
+		      DFService.register(this, dfd );
+		    }
+		    catch (FIPAException fe) { fe.printStackTrace(); }
+	  }
 
 }
