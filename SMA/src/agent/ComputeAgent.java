246e49c03bb6012fec46ebe082e413cf9e1d6e97
EnzoKalinowski <enzo.kalinowski@gmail.com>
2022-12-11 18:31:14 +0100

    ComputeAgent modif

--- SMA/src/agent/ComputeAgent.java
+++ SMA/src/agent/ComputeAgent.java
@@ -1,5 +1,39 @@
 package agent;
 
-public class ComputeAgent {
+import functions.Function;
+import functions.MyFunction;
+import jade.core.behaviours.*;
+import jade.core.Agent;
+import jade.lang.acl.*;
 
+public class ComputeAgent extends Agent{
+	protected Function f;
+	
+	protected void setup() {
+		addBehaviour(new CyclicBehaviour() {
+			
+			public void action() {
+				ACLMessage msg =receive();
+				if(msg!=null) {
+					System.out.println(" - "+ myAgent.getLocalName()+" <- "+ msg.getContent());
+					String[] splitMsg= msg.getContent().split(",");
+					
+					f=new MyFunction(Double.parseDouble(splitMsg[0]), Double.parseDouble(splitMsg[1]), Double.parseDouble(splitMsg[2]));
+				
+					double computeResult=f.eval();
+					
+					ACLMessage reply = msg.createReply();
+					reply.setPerformative(ACLMessage.INFORM);
+					reply.setContent(""+computeResult);
+					send(reply);
+				}
+				block();
+			}
+		});
+	}
+	
+	protected void takeDown() {
+		
+	}
+	
 }
