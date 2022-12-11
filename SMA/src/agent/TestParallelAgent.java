4f41e12455ad50dc03cc8a9ce9fb0ce82359a44a
ewenn-s <60225310+ewenn-s@users.noreply.github.com>
2022-12-11 16:49:36 +0100

    début du programme

---
+++ SMA/src/agent/TestParallelAgent.java
@@ -0,0 +1,20 @@
+package agent;
+
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.*;
+
+public class TestParallelAgent extends Agent{
+	protected void setup() {
+		addBehaviour(new CyclicBehaviour() {
+			
+			@Override
+			public void action() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+	
+
+}
