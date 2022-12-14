067ea7da25a84981d96e3526ce9a2ced2a5f729b
EnzoKalinowski <enzo.kalinowski@gmail.com>
2022-12-14 20:40:09 +0100

    ComputerAgent register services

--- SMA/src/agent/ComputeAgent.java
+++ SMA/src/agent/ComputeAgent.java
@@ -5,12 +5,29 @@ import functions.MyFunction;
 import jade.core.behaviours.*;
 import jade.core.Agent;
 import jade.lang.acl.*;
+import jade.core.*;
+import jade.domain.*;
+import jade.domain.FIPAAgentManagement.*;
 
 
 public class ComputeAgent extends Agent{
 	protected Function f;
 	
 	protected void setup() {
+		
+		ServiceDescription sd = new ServiceDescription();
+		
+		sd.setType("calculator");
+		sd.setName(getLocalName());
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+		
 		addBehaviour(new CyclicBehaviour() {
 			
 			public void action() {
@@ -23,11 +40,11 @@ public class ComputeAgent extends Agent{
 					double min = Double.parseDouble(splitMsg[1]);
 					double max = Double.parseDouble(splitMsg[2]);
 					double delta = Double.parseDouble(splitMsg[3]);
-					
+					System.out.println(splitMsg[0]);
 					switch (splitMsg[0]) {
 					
 						case "MyFunction": {
-							f=new MyFunction(Double.parseDouble(splitMsg[0]), Double.parseDouble(splitMsg[1]), Double.parseDouble(splitMsg[2]));
+							f=new MyFunction(min, max, delta);
 	
 							break;
 						}
@@ -40,6 +57,7 @@ public class ComputeAgent extends Agent{
 					ACLMessage reply = msg.createReply();
 					reply.setPerformative(ACLMessage.INFORM);
 					reply.setContent(""+computeResult);
+					System.out.println(reply);
 					send(reply);
 				}
 				block();
@@ -48,7 +66,12 @@ public class ComputeAgent extends Agent{
 	}
 	
 	protected void takeDown() {
+		ServiceDescription sd = new ServiceDescription();
 		
+		sd.setType("calculator");
+		sd.setName(getLocalName());
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.removeServices(sd);
 	}
 	
 }
