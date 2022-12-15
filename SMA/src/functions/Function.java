47830d639ed53d56c8f952cf896cbefa998eabdd
EnzoKalinowski <enzo.kalinowski@gmail.com>
2022-12-15 20:50:48 +0100

    javadoc comments for Function and MyFunction

--- SMA/src/functions/Function.java
+++ SMA/src/functions/Function.java
@@ -1,18 +1,34 @@
 package functions;
 
+
 public abstract class Function {
 	public final double min;
 	public final double max;
 	public final double delta;
 	
+	/**
+	 * 
+	 * @param min
+	 * @param max
+	 * @param delta
+	 */
 	public Function(double min, double max, double delta){
 		this.min=min;
 		this.max=max;
 		this.delta=delta;
 	}
 	
+	/**
+	 * 
+	 * @param x
+	 * @return f(x)
+	 */
 	public abstract double f(double x);
 	
+	/**
+	 * 
+	 * @return f(x) integral from min to max with a step of delta
+	 */
 	public double eval() {
 		double res=0;
 		for(double i=min;i<=max;i+=delta) {
--- SMA/src/functions/MyFunction.java
+++ SMA/src/functions/MyFunction.java
@@ -1,11 +1,26 @@
 package functions;
+import functions.Function;
 
+/**
+ * 
+ * @brief Function inherited class wich represent f(x)= x*x
+ *
+ */
 public class MyFunction extends Function {
-	
+	/**
+	 * 
+	 * @param min
+	 * @param max
+	 * @param delta
+	 */
 	public MyFunction(double min, double max, double delta) {
 		super(min, max, delta);
 	}
 
+	/**
+	 * @param x
+	 * @return x*x
+	 */
 	public double f(double x) {
 		return x*x;
 	}
